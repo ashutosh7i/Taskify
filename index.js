33fd130e36ee13094d9b2e10f46ab37713edb973
Isha dubey <90132738+ishadubey30@users.noreply.github.com>
2024-01-14 14:52:42 +0000

    .

--- index.js
+++ index.js
@@ -21,47 +21,6 @@ var currentSectionIndex = 0;
 hideAllSections();
 showCurrentSection();
 
-//dynamic content
-
-
-const dynamicContent = document.getElementById("dynamic-text");
-console.log(dynamicContent)
-const phrases = ["Hello..", "Tere.." , "Hola.." , "Namaste.." , "Shalom.."]
-
-let letterIndex = 0;
-let phraseIndex = 0;
-
-function printLetters(phrase){
-    if (letterIndex < phrase.length){
-        dynamicContent.textContent += phrase.charAt(letterIndex);
-        letterIndex += 1;
-        setTimeout(function(){
-            printLetters(phrase)}, 100)
-    }
-    else if(letterIndex== phrase.length){
-        clearletters()
-    }
-}
-function clearletters(){
- if(letterIndex > -1)
- { 
-    let updatedPhrase= "" ;
-    
-    for (let index = 0; index < letterIndex; index++) {
-        updatedPhrase += phrases[phraseIndex].charAt(index);
-      }
-    dynamicContent.textContent = updatedPhrase;
-    letterIndex -= 1;
-    setTimeout (clearletters , 100);
-    }
-  else if (letterIndex == -1)
-  { phraseIndex= (phraseIndex +1) % phrases.length;
-    letterIndex = 0;
-    printLetters(phrases[phraseIndex])
-}  
-
-}
-printLetters(phrases[phraseIndex])
 
 
 // create a simple instance
